c06089d63d4a22a4f562a31c0070ac7c8ba75f0b
videvid <vidar@videvit.se>
2023-05-22 01:02:21 +0200

    init default react and spring applications

---
+++ chili/src/test/java/com/chili/chili/ChiliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chili.chili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
