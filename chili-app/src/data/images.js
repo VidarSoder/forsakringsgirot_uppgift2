4a11e226c5aac112cd30e6f80780b29dca342262
videvid <vidar@videvit.se>
2023-05-22 01:28:07 +0200

    added post-api endpoint and persitant DB

---
+++ chili-app/src/data/images.js
@@ -0,0 +1,5 @@
+export const chiliImages = [
+    'https://cdn.shopify.com/s/files/1/2182/2603/files/redchilipepper.jpg?v=1669750479',
+    'https://i.redd.it/sc765watwjo51.jpg',
+    'https://i.ebayimg.com/images/g/FU8AAOSwjntg8tyi/s-l1600.jpg'
+]
